1b5251751aab1c521b16c9412adeff7399e0367b
Arkadiy Illarionov <qarkai@gmail.com>
2022-11-27 13:14:53 +0300

    Remove PostgreSQL requirement

--- pimpletv-srv.py
+++ pimpletv-srv.py
@@ -2,46 +2,19 @@
 
 import os
 
-import psycopg2
-
 from pimpletv_parser import get_broadcasts, get_playlist_entry
-
-
-DATABASE_URL = os.environ['DATABASE_URL']
 
 
 def get_playlist() -> str:
     playlist = '#EXTM3U\n'
+
     broadcasts = get_broadcasts()
+    for b in broadcasts:
+        print(f'{b.teams}, {b.channel}, {"Live" if b.live else b.time}, {b.link}')
+        m3u = get_playlist_entry(b)
+        if m3u:
+            playlist += m3u
 
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    try: 
-        with conn:
-            with conn.cursor() as curs:
-                curs.execute("DELETE FROM pimple WHERE query_ts < NOW() - INTERVAL '24 hours';")
-
-                if not broadcasts:
-                    curs.execute('SELECT m3u FROM pimple;')
-                    m3us = curs.fetchall()
-                    playlist += ''.join(m3u[0] for m3u in m3us)
-
-                for b in broadcasts:
-                    print(f'{b.teams}, {b.channel}, {"Live" if b.live else b.time}, {b.link}')
-                    SQL = 'SELECT m3u FROM pimple WHERE link = %s;'
-                    curs.execute(SQL, (b.link, ))
-                    m3u = curs.fetchone()
-                    if not m3u:
-                        m3u = get_playlist_entry(b)
-                        if not m3u:
-                            continue
-                        SQL = 'INSERT INTO pimple (link, m3u, query_ts) VALUES (%s, %s, NOW());'
-                        curs.execute(SQL, (b.link, m3u))
-                    else:
-                        m3u = m3u[0]
-
-                    playlist += m3u
-    finally:
-        conn.close()
     return playlist
 
 
